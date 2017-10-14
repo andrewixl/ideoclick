47e8a0d3beedfe9cdc8b6137eade56a5de815a38
Andrew Burger <burger.andrewixl@gmail.com>
2017-10-13 23:11:23 -0700

    Client Photos Database Update

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Client
 
 def index(request):
-   return render(request, 'main/index.html')
+    clients = Client.objects.all().order_by('client_name')
+    context = {
+        "client" : clients,
+    }
+    return render(request, 'main/index.html', context)
 
 def team(request):
-   return render(request, 'main/team.html')
+    return render(request, 'main/team.html')
