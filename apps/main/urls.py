7a504582e0103565729ffdd0418f10f8cd03c84d
Burger <BurgAnd18@issaquah.wednet.edu>
2018-01-09 10:00:03 -0800

    Homepage and Team Fixes

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     url(r'^team$', views.team),
     url(r'^clients$', views.clients),
     url(r'^fulfillment$', views.fulfillment),
-    url(r'^subscriptions$', views.subscriptions),
-    url(r'^estoremonetization$', views.estoremonetization),
+    url(r'^marketing$', views.marketing),
+    url(r'^consulting$', views.consulting),
     url(r'^demanddriver$', views.demanddriver),
     url(r'^itemmonitor$', views.itemmonitor),
-    url(r'^contentmanagement$', views.contentmanagement),
+    url(r'^contentdevelopment$', views.contentdevelopment),
 ]
--- apps/main/views.py
+++ apps/main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Client
+from .models import Client, Employee
 
 def index(request):
     clients = Client.objects.filter(client_type = 'large').all().order_by('client_name')
@@ -9,13 +9,19 @@ def index(request):
     return render(request, 'main/index.html', context)
 
 def team(request):
-    operations = Client.objects.filter(employee_type = "operations").all()
-    catalog = Client.objects.filter(employee_type = "catalog").all()
-    catalog = Client.objects.filter(employee_type = "catalog").all()
-    catalog = Client.objects.filter(employee_type = "catalog").all()
+    operations = Employee.objects.filter(employee_type = "operations").all()
+    catalog = Employee.objects.filter(employee_type = "catalog").all()
+    account_management = Employee.objects.filter(employee_type = "account_management").all()
+    development = Employee.objects.filter(employee_type = "development").all()
+    marketing = Employee.objects.filter(employee_type = "marketing").all()
+    support = Employee.objects.filter(employee_type = "support").all()
     context = {
-        "operations" : operations,  
+        "operations" : operations,
         "catalog" : catalog,
+        "account_management" : account_management,
+        "development" : development,
+        "marketing" : marketing,
+        "support" : support,
     }
     return render(request, 'main/team.html', context)
 
@@ -29,11 +35,11 @@ def clients(request):
 def fulfillment(request):
     return render(request, 'main/services/fulfillment.html')
 
-def subscriptions(request):
-    return render(request, 'main/services/subscriptions.html')
+def marketing(request):
+    return render(request, 'main/services/marketing.html')
 
-def estoremonetization(request):
-    return render(request, 'main/services/estoremonetization.html')
+def consulting(request):
+    return render(request, 'main/services/consulting.html')
 
 def demanddriver(request):
     return render(request, 'main/services/demanddriver.html')
@@ -41,5 +47,5 @@ def demanddriver(request):
 def itemmonitor(request):
     return render(request, 'main/services/itemmonitor.html')
 
-def contentmanagement(request):
-    return render(request, 'main/services/contentmanagement.html')
+def contentdevelopment(request):
+    return render(request, 'main/services/contentdevelopment.html')
